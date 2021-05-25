db8619fe46678a4e2451118ab277e3dc915a52e7
Joudrier Kevin <kev.joudrier@gmail.com>
2021-05-25 16:34:40 +0200

    Création du composant footer et modification de l'affichage du logo.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import Logo from '../Components/Logo';
+import Logo from '../Components/Logo/Logo'
+import Footer from '../Components/Footer/Footer';
 
 export default function Home() {
     return(
         <div>
             <Logo />
+            <Footer />
         </div>
     );
-}
+}    
