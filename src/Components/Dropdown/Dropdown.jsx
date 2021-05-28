0caf520185854578fa032f491b2f554469e2997e
Joudrier Kevin <kev.joudrier@gmail.com>
2021-05-28 15:26:39 +0200

    Ajouts des commentaires sur la classe et suppression de l'animation et aussi de la référence sur le composant.

--- src/Components/Dropdown/Dropdown.jsx
+++ src/Components/Dropdown/Dropdown.jsx
@@ -1,60 +1,104 @@
 import React from 'react';
 
+/**
+ * class Dropdwon
+ * ---------------
+ * Créer par Jodurier Kevin
+ * 
+ * @property {string} title Titre de la dropdown
+ * @property {ReactNode} children enfant du composant
+ * @property {{visible: boolean}} state État du composant
+ * 
+ **/
 class Dropdown extends React.Component {
 
     constructor(props) {
         super();
 
-        this.children = props.children ? props.children : null;
+        this.security(props);
 
+        this.children = props.children ? props.children : null;
+        this.state = { visible: false }
+        
+        this.bindEvents();
+    }
+
+    /**
+     * Sécurise le composant si il manque des propriété
+     * 
+     * @param {*} props 
+     **/
+    security(props) {
         if(props.title) {
             this.title = props.title;
         }
         else {
-            console.error("Il manque la propriété \"title\" sur le composant dropdown");
+            throw "The title property is required (title=\"My title\")";
         }
+    }
 
-        this.state = { visible: false }
-        this.ref = React.createRef();
-
+    /**
+     * Lie les événements avec la classe
+     * 
+     * @return {void}
+     **/
+    bindEvents() {
         this.onButtonVisible = this.onButtonVisible.bind(this);
     }
 
-    componentDidUpdate(){
-        this.changeIcon();
-    }
-
+    /**
+     * Change l'icone du bouton quand le status du composant change
+     * 
+     * @returns {string} Nouvelle icone
+     **/
     changeIcon() {
-        const span_element = this.ref.current.children[0].children[1].children[0];
         if(this.state.visible) {
-            span_element.classList.add('icon_arrow-down')
-            span_element.classList.remove('icon_arrow-up')
+            return 'icon icon_arrow-down';
         }
-        else{
-            span_element.classList.add('icon_arrow-up')
-            span_element.classList.remove('icon_arrow-down')
-        }
+
+        return 'icon icon_arrow-up';
     }
 
+    /**
+     * Change la classe du contenu quand le status du composant change
+     * 
+     * @returns {string} Nouvelle classes
+     **/
+    contentClassName() {
+        let className = 'dropdown__content ';
+        className += this.state.visible ? 'visible' : '';
+
+        return className;
+    }
+
+
+    /**
+     * Évenement du bouton qui permet de rendre visible le contenu
+     * 
+     * @param {Event} event 
+     **/
     onButtonVisible(event) {
         event.preventDefault();
         this.setState({visible: !this.state.visible})
-
-        const content_element = this.ref.current.children[1];
-        content_element.classList.toggle('visible');
-        this.changeIcon();
     }
 
+    /**
+     * Affcihe le composant dropdown
+     * 
+     * @returns {HTMLElement|null} Composant dropdown 
+     **/
     render() {
        if(this.title) {
-            return <div ref={this.ref} className="dropdown">
+            return <div className="dropdown">
                     <div className="dropdown__header">
                         <p className="dropdown__header-title">{this.title}</p>
-                        <button onClick={this.onButtonVisible}><span className="icon icon_arrow-up"></span></button>
+                        <button onClick={this.onButtonVisible}><span className={this.changeIcon()}></span></button>
                     </div>
 
-                    <div className="dropdown__content">
-                        { this.children }
+                    <div className={this.contentClassName()}>
+                        <div className="dropdown__content-wrapper">
+                            { this.children }
+                        </div>
                     </div>
             </div>;
        }
