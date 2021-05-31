28c63501c6c365be2002a1460dc39ea61df64c3e
Joudrier Kevin <kev.joudrier@gmail.com>
2021-05-31 13:36:06 +0200

    Feature composant Section
    * Création du composant section et tests unitaires

---
+++ src/Components/Section/Section.jsx
@@ -0,0 +1,49 @@
+import './section.scss';
+import React from 'react';
+
+class Section extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.src = props.src;
+        this.color = props.color;
+        this.text = props.text;
+    }
+
+    displayImage() {
+        if(this.src) {
+            return <img src={this.src} alt="section" />
+        }
+        
+        return null;
+    }
+
+    style() {
+        if(this.color) {
+            return {background:this.color}
+        }
+
+        return null;
+    }
+
+
+    displayText() {
+        if(this.text) {
+            return <p>{this.text}</p>;
+        }
+
+        return null;
+    }
+
+    render() { 
+        return (  
+            <div data-testid="background" className="section" style={this.style()}>
+                { this.displayImage() }
+
+                { this.displayText() }
+            </div>
+        );
+    }
+}
+ 
+export default Section;
