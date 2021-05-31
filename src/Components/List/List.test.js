3d79d601db8a9ed15dfc7209d71452bdd8c1e3dd
Joudrier Kevin <kev.joudrier@gmail.com>
2021-05-31 11:01:06 +0200

    Feature composant List
    * Création du composant list.
    
    * Ajout du tests d'affichage de la liste.
    
    * Réglage de la hauteur de ligne du texte
    
    * Modification de la hauteur de ligne sur mobile

---
+++ src/Components/List/List.test.js
@@ -0,0 +1,65 @@
+import { render } from "@testing-library/react"
+import { screen } from "@testing-library/dom"
+import List from './List';
+
+describe('Test Component List', () => {
+
+    it('should load without error', () => {
+        const list = [{text:'Mon super titre'}, {text: 'Mon second element'}, {text: 'Mon troisième element'}];
+        render(<List elements={list} />)
+    })
+
+    describe('Throw an exception', () => {
+        it('should throw an exception when the component does not contains property elements', () => {
+            let error = false;
+    
+            try {
+                render(<List />);
+            } catch(e) {
+                error = true;
+            }
+    
+            expect(error).toEqual(true);
+        })
+
+        it('should throw an exception not contains object in elements list', () => {
+            let error = false;
+
+            const list = ['Ma super list', 'test liste'];
+            
+            try {
+                render(<List elements={list} />);
+            } catch(e) {
+                error = true;
+            }
+    
+            expect(error).toEqual(true);
+        })
+
+        it('should throw an exception if object in elements list does not contain property text', () => {
+            let error = false;
+
+            const list = [{type: 'Ma liste'}];
+            
+            try {
+                render(<List elements={list} />);
+            } catch(e) {
+                error = true;
+            }
+    
+            expect(error).toEqual(true);
+        })
+    })
+
+    describe("Tests displaying list", () => {
+
+        it('The components should contains 3 elements', () => {
+            const list = [{text: 'My first element'}, {text: "My second element"}, {text: "My third element"}];
+            render(<List elements={list} />);
+            const elements = screen.getAllByRole("listitem");
+            expect(elements.length).toBe(3);
+        });
+
+    });
+   
+})
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Logo from '../Components/Logo/Logo'
 import Dropdown from '../Components/Dropdown/Dropdown'
 import Footer from '../Components/Footer/Footer';
+import List from '../Components/List/List';
 
 export default function Home() {
     const text = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed inventore commodi deserunt magnam illum neque aliquid, beatae minus voluptatum eaque est rerum consectetur minima qui amet veritatis esse impedit soluta nisi culpa exercitationem aperiam? Est expedita, ex aliquam reiciendis quae debitis enim ducimus explicabo repellendus eos tenetur minima ratione harum!";
@@ -15,15 +16,10 @@ export default function Home() {
             </Dropdown>
 
             <Dropdown title="Une autre box pour mon site">
-                <ul>
-                    <li>Climatisation</li>
-                    <li>Wi-Fi</li>
-                    <li>Cuisine</li>
-                    <li>Fer à repasser</li>
-                    <li>Sèche cheveux</li>
-                    <li>Cintres</li>
-                </ul>
+                <List elements={[{text: 'Climatisation'}, {text: "Wi-Fi"}, {text: "Cuisine"}]}/>
             </Dropdown>
+
+            
 
             <Footer />
         </div>
