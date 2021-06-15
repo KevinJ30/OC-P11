a1a08e6330635ca8c6ec72fd96b87377e9c4f16e
KevinJ30 <kev.joudrier@gmail.com>
2021-06-15 19:32:39 +0200

    Ajouts des données du fichier json.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,19 +5,41 @@ import Thumb from '../Components/Thumb/Thumb';
 class Home extends React.Component {
     constructor(props) {
         super(props);
+        
+        this.state = {
+            dataIsLoaded: false,
+            data: []
+        }
+    }
+
+    // Chargement des données depuis un fichier JSON
+    componentDidMount() {
+        fetch('/logements.json')
+            .then(response => response.json())
+            .then((data) => {
+                
+                this.setState({
+                    dataIsLoaded: true,
+                    data: data
+                });
+
+            });
     }
     
     render() { 
         return <div className="home">
             <Section src="/medias/image_section.jpg" text="Chez vous, partout et ailleurs" />
 
-            <section className="home__gallery mt-">
-                <Thumb title="My good location" url_img="http://lorempixel.com/600/400/" />
-                <Thumb title="location very fast" url_img="http://lorempixel.com/600/400/" />
-                <Thumb title="small Durty" url_img="http://lorempixel.com/600/400/" />
-                <Thumb title="Big location" url_img="http://lorempixel.com/600/400/" />
-                <Thumb title="My first good location" url_img="http://lorempixel.com/600/400/" />
-                <Thumb title="Very Good" url_img="http://lorempixel.com/600/400/" />
+            <section className="home__gallery">
+                {
+                    !this.state.dataIsLoaded && 
+                    <span className="home__loading_data">Chargement en cours...</span>
+                }
+
+                {
+                    this.state.dataIsLoaded && 
+                    this.state.data.map(property => <Thumb title={property.title} url_img={property.cover} />)
+                }
             </section>
         </div>;
     }
