b3b7b2b030695b8a071e43878cdf665f13fd3533
Joudrier Kevin <kev.joudrier@gmail.com>
2021-06-21 11:25:26 +0200

    Feature optimization. (#25)

--- src/Components/Thumb/Thumb.jsx
+++ src/Components/Thumb/Thumb.jsx
@@ -10,15 +10,50 @@ class Thumb extends React.Component {
             const err = Error('The component doesn\'t contain url_img property');
             throw err;
         }
+
+        this.state = {
+            visible: false,
+            observer: null
+        }
+
+        this.ref = React.createRef();
+        this.handleVisibility = this.handleVisibility.bind(this);
     }
 
-    render() { 
-        return <div className="thumb filter">
-            { 
+    componentDidMount() {
+        const options = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.5
+        }
+
+        const observer = new IntersectionObserver(this.handleVisibility, options)
+
+        if(this.ref.current) {
+            observer.observe(this.ref.current);
+        }
+    }
+
+    handleVisibility(entries) {
+        const [entry] = entries;
+
+        if(entry.isIntersecting) {
+            this.setState({
+                visible: true
+            });
+        }
+    }
+
+    render() {
+        return <div ref={this.ref} className="thumb filter">
+            {
                 this.props.title &&
                 <h3 className="thumb__title">{this.props.title}</h3>
             }
-            <img className="thumb__img" src={this.props.url_img} alt="bg_thumb" />
+
+            {
+                this.state.visible && <img className="thumb__img" src={this.props.url_img} alt="bg_thumb" />
+            }
        </div>;
     }
 }
