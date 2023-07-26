c79d24d0de6fc5e8a78d09953aae5e2f9fdd37dc
KevinJ30 <kev.joudrier@gmail.com>
2023-07-26 19:48:54 +0200

    Updates

--- src/Tests/asserts.js
+++
@@ -1,8 +0,0 @@
-import {render} from "@testing-library/react";
-
-export function assertThrowComponent(component, messageException) {
-    const err = console.error;
-    console.error = jest.fn();
-    expect(() => render(component)).toThrowError(messageException);
-    console.error = err;
-}
